e0508f3a8bd6aac0d6315960613544b5a12ec5cd
Lethabo <instinctslump@gmail.com>
2025-01-15 03:53:07 +0200

    new api key

--- stedi_ai_config.php
+++ stedi_ai_config.php
@@ -1,5 +1,5 @@
 <?php
-define('GEMINI_API_KEY', 'AIzaSyC8N6WCtOpRXQJa8JcGJ01CFlioasdK8Q0');
+define('GEMINI_API_KEY', 'AIzaSyAba_AYaF9mPzG0GyBNjUNGThx9L1C_Z_4');
 define('GEMINI_API_URL', 'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent');
 
 define('QDRANT_URL', 'https://344b1416-5371-4c71-80a0-3776cfab59ea.europe-west3-0.gcp.cloud.qdrant.io:6333');
